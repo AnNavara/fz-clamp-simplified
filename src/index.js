f2f17b1c41637f36fddb4ca91d15f53c0e2fea34
Vyacheslav Serdyuk <Navarachkou@gmail.com>
2022-03-21 23:51:12 +0500

    add styles and layout

--- src/index.js
+++ src/index.js
@@ -1,2 +1,7 @@
-const newVar = 'new variable';
-console.log('and now this')
+// Get input strings
+// 2-way bind inputs
+// Parse and save input strings
+// Change css clamp() to provided values in examples
+
+// calculate clamp values for list of checkpoints in px and provide to Chart.js
+// when values of inputs change run parse-save and update Chart.js values
